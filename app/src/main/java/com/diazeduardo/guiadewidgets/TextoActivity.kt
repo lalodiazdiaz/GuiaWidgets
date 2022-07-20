2713c6f0740289898e6eaa218a921bba838c68eb
lalodiazdiaz <eduardodiazdiaz93@gmail.com>
2022-07-20 16:16:22 -0500

    Add funcionality to text activity

--- app/src/main/java/com/diazeduardo/guiadewidgets/TextoActivity.kt
+++ app/src/main/java/com/diazeduardo/guiadewidgets/TextoActivity.kt
@@ -2,10 +2,20 @@ package com.diazeduardo.guiadewidgets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_texto.*
+
 
 class TextoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_texto)
+
+        btnMostrarDatos.setOnClickListener {
+            var nombre = txtNombre.text.toString()
+            var edad = txtEdad.text.toString()
+            Toast.makeText(applicationContext, "Que onda $nombre, con que tienes $edad",Toast.LENGTH_LONG).show()
+
+        }
     }
 }
